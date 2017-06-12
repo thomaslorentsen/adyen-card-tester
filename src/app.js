0c94d60d1157ed02791812886f19aeca128e8658
Tom Lorentsen <tom@thomaslorentsen.co.uk>
2017-06-12 21:10:15 +0100

    Fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
-var apiKey = process.env.ADYEN_KEY;
+var apiKey = process.env.ADYEN_KEY
 
 var express = require('express'),
     app = express();
-
-var hbs = require('hbs');
-require('handlebars-form-helpers').register(hbs.handlebars);
 
 var exphbs = require('express-handlebars')
 app.engine('.hbs', exphbs({defaultLayout: 'single', extname: '.hbs'}))
 app.set('view engine', '.hbs')
 
-HandlebarsFormHelpers.register(Handlebars);
+require('handlebars-form-helpers').register(exphbs.handlebars);
 
-var path = require('path');
+var path = require('path')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use('/node_modules', express.static('node_modules'))
